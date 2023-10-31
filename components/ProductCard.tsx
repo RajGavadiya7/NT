f3555deab973902f250ebae5a763012fe09b003c
Aniket Rana <76243585+Aniumbott@users.noreply.github.com>
2023-10-31 22:51:15 +0530

    Box shadow added for depth look. (#10)

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -6,7 +6,7 @@ export default function ProductCard(data: any) {
   // Card data object
   const dataProduct = data.data;
   return (
-    <Card shadow="sm" p="lg" radius="md" withBorder>
+    <Card shadow="sm" p="lg" radius="md" withBorder style={{boxShadow: "0 0 50px #00000039"}}>
       <Card.Section>
         <Image
           src={dataProduct.url}
@@ -18,7 +18,7 @@ export default function ProductCard(data: any) {
 
       <Group position="apart" mt="md" mb="xs">
         <Text weight={500}>{dataProduct.productname}</Text>
-        <Badge color="green"  variant="light" size="xl">
+        <Badge color="green" variant="light" size="xl">
           {" ₹ " + dataProduct.price}
         </Badge>
       </Group>
